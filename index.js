3dc7518c51412e676c310b20022c64f2813a452a
Eve Kozina <eve.kozina@gmail.com>
2021-10-15 16:01:18 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push (`${name}`);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift (`${name}`);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop ();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift ();
+}
+
+function appendCat(name) {
+    const copyOfCats = [...cats, `${name}`];
+    return copyOfCats;
+}
+
+function prependCat(name) {
+    const copyOfCats = [`${name}`, ...cats];
+    return copyOfCats;
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
